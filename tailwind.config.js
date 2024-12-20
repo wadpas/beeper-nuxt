20e249d9a33e5eb71ad21cbe8d2a04639672d9a8
wadpas <wadpas@gmail.com>
2024-12-20 10:34:52 +0200

    install expo & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-export default {
-  content: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
